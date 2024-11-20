07ef5968fd8e057dbed30b4b7522495560d36f11
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-20 12:05:34 +0530

    test

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-b74"
-    key    = "aws-parameters/terraform.tfstate"
+    key    = "aws-parameter/terraform.tfstate"
     region = "us-east-1"
   }
 }
